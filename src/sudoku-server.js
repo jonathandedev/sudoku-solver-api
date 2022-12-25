7a59bf649842514920277aa1d64aa45f3ae30bf1
Jonathan E <admin@jonathane.dev>
2022-12-25 13:04:27 +0000

    Allow requests
    The API will now receive requests responding with no sudoku being sent.

--- src/sudoku-server.js
+++ src/sudoku-server.js
@@ -7,4 +7,18 @@ const express = require("express");
 const app = express();
 const port = 1604;
 
+// Server Setup
 app.listen(port, () => console.log("Listening on port: " + port));
+
+app.get("/api/solveSudoku", (req, res) => {
+    console.log("Received request: Solve sudoku");
+
+    try {
+        throw 0;
+    } catch (err) {
+        if (err == 0) {
+            res.status(400);
+            res.send("No Sudoku Sent");
+        }
+    }
+});
