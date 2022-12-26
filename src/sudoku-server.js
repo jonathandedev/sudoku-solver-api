018f0b9c7e9f35dba9a0ce2f453905a3ffe11ecb
Jonathan E <admin@jonathane.dev>
2022-12-26 22:19:08 +0000

    Convert string to 2D array
    I created a function createGrid that converts the string attribute in the sudoku object into a 2D array.

--- src/sudoku-server.js
+++ src/sudoku-server.js
@@ -21,18 +21,43 @@ app.get("/api/solveSudoku/:sudoku", (req, res) => {
     console.log("Received request: Solve sudoku");
 
     try {
-        checkSudokuFormat(req.params.sudoku);
+        const sudoku = JSON.parse(req.params.sudoku);
+
+        checkSudokuFormat(sudoku);
+        solveSudoku(sudoku);
     } catch (err) {
+        console.log(err);
         res.status(400);
         res.send(err);
     }
 });
 
-function checkSudokuFormat(sent) {
-    if (sent == null || sent == undefined) throw "no sudoku sent";
-
-    const sudoku = JSON.parse(sent);
+function checkSudokuFormat(sudoku) {
+    if (sudoku == null || sudoku == undefined) throw "no sudoku sent";
 
     if (sudoku.puzzle == undefined || sudoku.puzzle == null) throw "the sudoku does not contain a puzzle";
+    if (typeof sudoku.puzzle != "string") throw "the sudoku does not contain a puzzle of the right format";
+
     if (sudoku.puzzle.trim().length != 81) throw "the sudoku puzzle is not the right size"
+}
+
+function solveSudoku(sudoku) { 
+    let grid = createGrid(sudoku.puzzle);
+}
+
+function createGrid(puzzle) {
+    const length = 9;
+
+    let grid = [];
+ 
+    for (let i=0; i<length; i++) {
+        let row = [];
+        for (let j=0; j<length; j++) {
+            let index = (i*length) + j;
+            row.push(puzzle.split("")[index]);
+        }
+        grid.push(row);
+    }
+
+    return grid;
 }
