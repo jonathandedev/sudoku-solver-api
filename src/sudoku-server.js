66bdc7406fdece2c2f9ca2ffbff0426162d55dae
Jonathan E <admin@jonathane.dev>
2022-12-27 19:09:49 +0000

    Add comments
    I added comments to the different functions.

--- src/sudoku-server.js
+++ src/sudoku-server.js
@@ -15,8 +15,10 @@ app.get("/api/solveSudoku/:sudoku", (req, res) => {
     console.log("Received request: Solve sudoku");
 
     try {
+        // Parse the client input
         const sudoku = JSON.parse(req.params.sudoku);
 
+        // Check, solve and send solution
         checkSudokuFormat(sudoku);
         let solution = solveSudoku(sudoku);
         res.send(solution);
@@ -27,6 +29,13 @@ app.get("/api/solveSudoku/:sudoku", (req, res) => {
     }
 });
 
+/**
+ * Checks the given JSON object isn't null or undefined.
+ * The object should have a puzzle attribute describing a sudoku puzzle in a single string.
+ * The error thrown is sent to the client.
+ * 
+ * @param {Object} sudoku the JSON object holding the puzzle string
+ */
 function checkSudokuFormat(sudoku) {
     if (sudoku == null || sudoku == undefined) throw "no sudoku sent";
 
@@ -36,10 +45,21 @@ function checkSudokuFormat(sudoku) {
     if (sudoku.puzzle.trim().length != 81) throw "the sudoku puzzle is not the right size"
 }
 
+/**
+ * Used to convert the puzzle string into a 2D array.
+ * The 2D array is then solved and added to a solution object.
+ * A boolean of solvable is also added to the solution object.
+ * 
+ * @param {Object} sudoku the JSON object holding the puzzle string
+ * @returns the object holding the solvable boolean and potentially the solution
+ */
 function solveSudoku(sudoku) { 
+    // Convert string to 2D array
     let grid = createGrid(sudoku.puzzle);
+    // Solve the grid
     let solvable = bruteForce(grid);
     
+    // Create the solution object
     let solution = {
         solvable:solvable
     }
@@ -47,11 +67,16 @@ function solveSudoku(sudoku) {
     return solution;
 }
 
+/**
+ * Converts a string of length 81 into a 2D array.
+ * 
+ * @param {string} puzzle the string of length 81
+ * @returns the 2D array
+ */
 function createGrid(puzzle) {
     const length = 9;
 
     let grid = [];
- 
     for (let i=0; i<length; i++) {
         let row = [];
         for (let j=0; j<length; j++) {
@@ -64,6 +89,12 @@ function createGrid(puzzle) {
     return grid;
 }
 
+/**
+ * A recursive algorithm used to brute force solve a sudoku puzzle using backtracking.
+ * 
+ * @param {Array} grid the 2D array representation of the sudoku 
+ * @returns a boolean on whether the puzzle is solvable
+ */
 function bruteForce(grid) {
     for (let row = 0; row < 9; row++) {
         for (let col = 0; col < 9; col++) {
@@ -84,14 +115,26 @@ function bruteForce(grid) {
     return true;
 }
 
+/**
+ * Determines whether a number inputted into a sudoku would a allowed based on standard rules.
+ * 
+ * @param {Array} grid the 2D array representation of the sudoku
+ * @param {*} row the current row
+ * @param {*} col the current column
+ * @param {*} num the number to be inserted
+ * @returns whether the given number can be inserted
+ */
 function valid(grid, row, col, num) {
+    // Checks the same number isn't in the current row
     for (let i=0; i<9; i++) {
         if (grid[row][i] == num) return false;
     }
+    // Checks the same number isn't in the current column
     for (let i=0; i<9; i++) {
         if (grid[i][col] == num) return false;
     }
 
+    // Checks the smaller 3x3 doesn't contain the same number
     for (let r=Math.floor(row/3)*3; r<Math.floor(row/3)*3 + 3; r++) {
         for (let c=Math.floor(col/3)*3; c<Math.floor(col/3)*3 + 3; c++) {
             if (grid[r][c] == num) return false;
